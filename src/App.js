b7e61f5c76d040c94b680f0acc68a24b7318dd24
Denis Zana <denis.zana@gmail.com>
2019-11-20 11:55:01 +0100

    marked react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import { sampleText }   from './sampleText';
+import marked           from 'marked';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+// function App() {
+  state = {
+    text: sampleText
+  };
+  
+  componentWillMount = () => {
+    const localStorageText = localStorage.getItem( 'text' );
+    if( localStorageText     && typeof localStorageText !== 'undefined'       && localStorageText.replace(/\s/g,'').length > 0 ) {
+        this.setState( { text: localStorageText } );
+    }
+  }
+  
+  componentWillUpdate = ( nextProps, nextState ) => {
+    localStorage.setItem( 'text', nextState.text );
+  }
+  
+  editText =  ( event ) => {
+    this.setState({
+        text: event.target.value
+    });
+  }
+  
+  renderText = () => {
+    return {
+        __html: marked( this.state.text, { sanitize: true } )
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+          <div className="row">
+              <div className="col-sm-6">
+                  <textarea
+                      value={ this.state.text }
+                      rows="35"
+                      className="form-control"
+                      onChange={ this.editText.bind( this ) }
+                  />
+              </div>
+              <div className="col-sm-6">
+                  <div dangerouslySetInnerHTML={ this.renderText() }/>
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/*
+  state = {
+    text: sampleText
+  };
+  
+  componentWillMount() {
+    const localStorageText = localStorage.getItem( 'text' );
+    if( localStorageText     && typeof localStorageText !== 'undefined'       && localStorageText.replace(/\s/g,'').length > 0 ) {
+        this.setState( { text: localStorageText } );
+    }
+  }
+  
+  componentWillUpdate( nextProps, nextState ) {
+    localStorage.setItem( 'text', nextState.text );
+  }
+  
+  editText( event ) {
+    this.setState({
+        text: event.target.value
+    });
+  }
+  
+  renderText() {
+    return {
+        __html: marked( this.state.text, { sanitize: true } )
+    };
+  }
+  
+*/
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
